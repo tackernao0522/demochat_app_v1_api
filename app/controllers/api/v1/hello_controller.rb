ff413a3b0a0c53d4662702aaebfa58a1efbc5d07
takaki <takaki55730317@gmail.com>
2024-06-10 14:26:20 +0900

    add .rubocop.yml auto_format

--- app/controllers/api/v1/hello_controller.rb
+++ app/controllers/api/v1/hello_controller.rb
@@ -1,5 +1,11 @@
-class Api::V1::HelloController < ApplicationController
-    def index
-      render json: 'Hello'
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class HelloController < ApplicationController
+      def index
+        render json: 'Hello'
+      end
     end
+  end
 end
