a2d8dd18d1039d186832112beca20530f6f9f119
takaki <takaki55730317@gmail.com>
2024-06-12 11:25:07 +0900

    modify production

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # ルート`/`をウェルカムページに設定
+  root 'welcome#index'
+
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'auth/registrations'
   }
+
+  # 他のAPIエンドポイントのルート設定
+  get '/your_api_endpoint', to: 'api#your_action'
 end
