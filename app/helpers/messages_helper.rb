07bb8b438caef05b9d684937edff7e8ce9ec1acc
takaki <takaki55730317@gmail.com>
2024-06-17 12:35:27 +0900

    fix: conflict messages_helper.rb

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 module MessagesHelper
-<<<<<<< HEAD
-=======
   def format_message(message)
     "#{message.user.name}: #{message.content}"
   end
->>>>>>> main
 end
