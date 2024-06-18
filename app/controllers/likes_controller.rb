3535e6dc151ddcf58cc96ce3caa18db02fff939c
takaki <takaki55730317@gmail.com>
2024-06-19 00:44:13 +0900

    Refactor RSpec tests and add Japanese translations
    - Split RSpec blocks to comply with Rubocop limits
    - Add Japanese translations for error messages
    - Confirm all tests passing
    - Resolve all Rubocop warnings

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class LikesController < ApplicationController
-  before_action :authenticate_user!, only: ['create']
+  before_action :authenticate_user!, only: %w[create destroy]
 
   def create
     like = Like.new(message_id: params[:id], user_id: current_user.id)
@@ -10,6 +10,18 @@ class LikesController < ApplicationController
       render json: { id: like.id, email: current_user.email, message: '成功しました' }, status: :ok
     else
       render json: { message: '保存出来ませんでした', errors: like.errors.messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    like = Like.find_by(id: params[:id], user_id: current_user.id)
+
+    if like.nil?
+      render json: { message: '見つかりませんでした' }, status: :not_found
+    elsif like.destroy
+      render json: { id: like.id, email: like.user.email, message: '削除に成功しました' }, status: :ok
+    else
+      render json: { message: '削除できませんでした', errors: like.errors.messages }, status: :bad_request
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,29 @@
 # frozen_string_literal: true
 
+# db/seeds.rb
+
+# ユーザーを作成
+user = User.first_or_create!(
+  name: 'デフォルトユーザー',
+  email: 'default@example.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+if user.persisted?
+  Rails.logger.debug { "ユーザーが作成されました: #{user.id}" }
+else
+  Rails.logger.debug 'ユーザーの作成に失敗しました'
+end
+
+# メッセージを作成
 3.times do |number|
-  Message.create(content: "#{number}番目のメッセージです!", user_id: User.first.id)
-  Rails.logger.debug { "#{number}番目のメッセージを作成しました" }
+  message = Message.create!(content: "#{number}番目のメッセージです!", user_id: user.id)
+  if message.persisted?
+    Rails.logger.debug { "#{number}番目のメッセージが作成されました: #{message.id}" }
+  else
+    Rails.logger.debug { "#{number}番目のメッセージの作成に失敗しました" }
+  end
 end
 
 Rails.logger.debug 'メッセージの作成が完了しました'
