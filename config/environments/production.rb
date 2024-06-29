173a9a7e1c4d5a962334a27bbf5ce5eae8f223aa
takaki <takaki55730317@gmail.com>
2024-06-29 18:16:39 +0900

    feat: configure ActionCable for production environment
    - Update config/environments/production.rb to set ActionCable URL and allowed request origins
    - Ensure WebSocket connections are correctly handled in the production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,8 +37,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://demochat-api.fly.dev/cable'
+  config.action_cable.allowed_request_origins = ['https://front-sigma-three.vercel.app',
+                                                 %r{https://front-sigma-three\.vercel\.app.*}]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
