d8e2ac51b4f912abb70f2ba625f09c0559d9ad80
takaki <takaki55730317@gmail.com>
2024-07-06 17:27:05 +0900

    modify session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :cookie_store, key: '_app_session', secure: Rails.env.production?, httponly: true
+Rails.application.config.session_store :cookie_store,
+                                       key: '_app_session',
+                                       secure: Rails.env.production?,
+                                       httponly: true,
+                                       same_site: :none,
+                                       domain: :all
