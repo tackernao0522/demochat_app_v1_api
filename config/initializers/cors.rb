201b9e219ca54ccd4159673d6b06bc1bebf58f60
takaki <takaki55730317@gmail.com>
2024-07-06 18:31:45 +0900

    modify onfig/environments/production.rb, config/initializers/cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,15 +1,8 @@
 # frozen_string_literal: true
 
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['API_DOMAIN'] || '', 'https://front-sigma-three.vercel.app'
+    origins ENV.fetch('API_DOMAIN', nil), 'https://front-sigma-three.vercel.app'
 
     resource '*',
              headers: :any,
