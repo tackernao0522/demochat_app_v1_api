612667032131e0cbe11761e459328a4df900e9c9
takaki <takaki55730317@gmail.com>
2024-07-09 14:10:46 +0900

    fix: cors cross-site-origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,10 @@
 # frozen_string_literal: true
 
-# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins = ['https://front-sigma-three.vercel.app', 'http://localhost:3000']
-    origins << ENV['API_DOMAIN'] if ENV['API_DOMAIN'].present?
-
-    origins origins.compact.uniq
-
+    origins 'http://localhost:8080', 'https://front-sigma-three.vercel.app', 'https://demochat-api.fly.dev'
     resource '*',
              headers: :any,
-             expose: %w[access-token expiry token-type uid client],
              methods: %i[get post put patch delete options head],
              credentials: true
   end
